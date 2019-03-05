00f9f3ebbdf982ccf5823f742cb684f18b46b017
initkfs <none@none>
2019-03-05 22:44:41 +0300

    Extract cli module, remove iso image from repository

--- src/kernel.d
+++ src/kernel.d
@@ -11,27 +11,10 @@ import os.core.io.interrupt.isr;
 import os.core.io.interrupt.irq;
 import os.core.io.interrupt.pic;
 import os.core.memory.allocators.linear;
+import os.core.sys.cli;
 import os.core.sys.exit;
 
 extern (C) __gshared ulong KERNEL_END;
-
-private __gshared bool isActiveShell = false;
-private __gshared ubyte[100] shellCommandBuffer = [];
-private __gshared ShellCommand[1] shellCommands;
-
-struct ShellCommand
-{
-	string name;
-	string desctiption;
-	void function(immutable(char[]) args) action;
-
-	this(string name, string desctiption, void function(immutable(char[]) args) action)
-	{
-		this.name = name;
-		this.desctiption = desctiption;
-		this.action = action;
-	}
-}
 
 extern (C) void kmain(uint magic, size_t* multibootInfoAddress)
 {
@@ -50,11 +33,11 @@ extern (C) void kmain(uint magic, size_t* multibootInfoAddress)
 	setIRQs();
 	setIDT();
 
-	auto exitCommand = ShellCommand("exit", "Immediate shutdown", &exitNow);
+	auto exitCommand = CliCommand("exit", "Immediate shutdown", &exitNow);
 
-	shellCommands[0] = exitCommand;
+	cliCommands[0] = exitCommand;
 
-	isActiveShell = true;
+	enableCli();
 	kprintln("Shell has started. Enter the command");
 	printCmd();
 }
@@ -92,12 +75,6 @@ public extern (C) __gshared void runInterruptRequest(ulong num, ulong err)
 	sendEndPIC1();
 }
 
-private void printCmd()
-{
-	enableCursor();
-	kprint("$>");
-}
-
 private void scanCode()
 {
 
@@ -107,98 +84,8 @@ private void scanCode()
 		return;
 	}
 
-	if (k == '\n' && isActiveShell)
-	{
-		for (int i = 0; i < shellCommandBuffer.length; i++)
-		{
-			if (shellCommandBuffer[i] == 0u || shellCommandBuffer[i] == ' ')
-			{
-
-				if (i == 0)
-				{
-					//command is empty
-					return;
-				}
-
-				disableCursor();
-				parseAndRunCommand(shellCommandBuffer[0 .. i], shellCommandBuffer[i .. $]);
-				break;
-			}
-		}
-
-		for (int i = 0; i < shellCommandBuffer.length; i++)
-		{
-			shellCommandBuffer[i] = 0u;
-		}
-
-		printCmd();
-	}
-	else if (k == '\t' && isActiveShell)
-	{
-		printHelp();
-	}
-	else if (isActiveShell)
-	{
-		enableCursor();
-		for (int i = 0; i < shellCommandBuffer.length; i++)
-		{
-			ubyte c = shellCommandBuffer[i];
-			if (c == 0u)
-			{
-				shellCommandBuffer[i] = k;
-				break;
-			}
-		}
-		char[1] charStr = [k];
-		kprint(cast(string) charStr);
-	}
-}
-
-private void printHelp()
-{
-	disableCursor();
-	kprintln("");
-	kprintln("Available commands:");
-	foreach(int index, ShellCommand cmd; shellCommands){
-		string[2] cmdInfo = [cmd.name, cmd.desctiption];
-		kprintfln!string("%s - %s", cmdInfo);
-	}
-
-	printCmd();
-}
-
-private void parseAndRunCommand(ubyte[] command, ubyte[] args)
-{
-
-	immutable(string) commandStr = cast(immutable(string)) command;
-	immutable(char[]) argsArray = cast(immutable(char[])) args;
-
-	outer: foreach (size_t index, ref ShellCommand cmd; shellCommands)
-	{
-
-		string shellName = cmd.name;
-
-		if (shellName.length != command.length)
-		{
-			continue;
-		}
-		else
-		{
-			for (int ii = 0; ii < shellName.length; ii++)
-			{
-				if (shellName[ii] != command[ii])
-				{
-					continue outer;
-				}
-			}
-		}
-
-		auto onAction = cmd.action;
-		onAction(argsArray);
+	if(isCliEnabled()){
+		applyForCli(k);
 		return;
 	}
-
-	kprintln("");
-	string[1] invalidCommand = [commandStr];
-	kprintfln!string("Not found command: %s", invalidCommand);
 }
---
+++ src/os/core/sys/cli.d
@@ -0,0 +1,144 @@
+module os.core.sys.cli;
+
+import os.core.graphic.display;
+import os.core.io.kstdio;
+
+private __gshared ubyte[100] shellCommandBuffer = [];
+
+private __gshared bool isActiveShell = false;
+private __gshared ubyte[100] cliCommandBuffer = [];
+//TODO remove public access
+public __gshared CliCommand[1] cliCommands;
+
+struct CliCommand
+{
+    string name;
+    string desctiption;
+    void function(immutable(char[]) args) action;
+
+    this(string name, string desctiption, void function(immutable(char[]) args) action)
+    {
+        this.name = name;
+        this.desctiption = desctiption;
+        this.action = action;
+    }
+}
+
+void enableCli()
+{
+    isActiveShell = true;
+}
+
+void disableCli()
+{
+    isActiveShell = false;
+}
+
+bool isCliEnabled(){
+    return isActiveShell;
+}
+
+void printCmd()
+{
+    enableCursor();
+    kprint("$>");
+}
+
+private void printHelp()
+{
+    disableCursor();
+    kprintln("");
+    kprintln("Available commands:");
+    foreach (int index, CliCommand cmd; cliCommands)
+    {
+        string[2] cmdInfo = [cmd.name, cmd.desctiption];
+        kprintfln!string("%s - %s", cmdInfo);
+    }
+
+    printCmd();
+}
+
+void applyForCli(char k)
+{
+    if (k == '\n' && isActiveShell)
+    {
+        for (int i = 0; i < cliCommandBuffer.length; i++)
+        {
+            if (cliCommandBuffer[i] == 0u || cliCommandBuffer[i] == ' ')
+            {
+
+                if (i == 0)
+                {
+                    //command is empty
+                    return;
+                }
+
+                disableCursor();
+                parseAndRunCommand(cliCommandBuffer[0 .. i], cliCommandBuffer[i .. $]);
+                break;
+            }
+        }
+
+        for (int i = 0; i < cliCommandBuffer.length; i++)
+        {
+            cliCommandBuffer[i] = 0u;
+        }
+
+        printCmd();
+    }
+    else if (k == '\t' && isActiveShell)
+    {
+        printHelp();
+    }
+    else if (isActiveShell)
+    {
+        enableCursor();
+        for (int i = 0; i < cliCommandBuffer.length; i++)
+        {
+            ubyte c = cliCommandBuffer[i];
+            if (c == 0u)
+            {
+                cliCommandBuffer[i] = k;
+                break;
+            }
+        }
+        char[1] charStr = [k];
+        kprint(cast(string) charStr);
+    }
+}
+
+private void parseAndRunCommand(ubyte[] command, ubyte[] args)
+{
+
+    immutable(string) commandStr = cast(immutable(string)) command;
+    immutable(char[]) argsArray = cast(immutable(char[])) args;
+
+    outer: foreach (size_t index, ref CliCommand cmd; cliCommands)
+    {
+
+        string cliCommandName = cmd.name;
+
+        if (cliCommandName.length != command.length)
+        {
+            continue;
+        }
+        else
+        {
+            for (int ii = 0; ii < cliCommandName.length; ii++)
+            {
+                if (cliCommandName[ii] != command[ii])
+                {
+                    continue outer;
+                }
+            }
+        }
+
+        auto onAction = cmd.action;
+        onAction(argsArray);
+        return;
+    }
+
+    kprintln("");
+    string[1] invalidCommand = [commandStr];
+    kprintfln!string("Not found command: %s", invalidCommand);
+}
